d57fde5080df31f90f1b999c2e0aea7fc599e5f0
shangzw <sahngzw12@163.com>
2016-03-16 21:14:07 +0800

    -

--- func/nav.php
+++ func/nav.php
@@ -7,16 +7,16 @@
 						<a href="http://www.shangzw.me">主页</a>
 					</li>
 					<li>
-						<a href="http://www.shangzw.me/sayings">说说</a>
+						<a href="http://www.shangzw.me/saying.php">说说</a>
 					</li>
 					<li>
-						<a href="http://www.shangzw.me/passages">所思</a>
+						<a href="http://www.shangzw.me/passages.php">所思</a>
 					</li>
 					<li>
-						<a href="http://www.shangzw.me/collections">不错哦</a>
+						<a href="http://www.shangzw.me/collections.php">不错哦</a>
 					</li>
 					<li>
-						<a href="http://www.shangzw.me/say_something">说点啥</a>
+						<a href="http://www.shangzw.me/say_something.php">说点啥</a>
 					</li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
