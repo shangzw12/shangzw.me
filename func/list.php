a8e7ed8b8d8d82be9a8be1022122ead03bf7d1b0
shangzw <sahngzw12@163.com>
2016-03-18 15:27:04 +0800

    --

--- func/list.php
+++ func/list.php
@@ -4,24 +4,24 @@
 	if(!$con) { die('Connect failed!\n').mysql_error;}
 	mysql_select_db($db_name, $con);
 
-	$sql = 'SELECT distinct date_format(date_sub(creat_time, interval dayofmonth(creat_time) - 1 day),"%y-%m-%d")
+	$sql = 'SELECT distinct date_format(date_sub(creat_time, interval dayofmonth(creat_time) - 1 day),"%Y-%m-%d")
 	 from saying';
 	//get 201x-xx-xx type date
 	$res = mysql_query($sql , $con);
 	while($one_time = mysql_fetch_array($res)){
 		//选择当前时间点下的所有的东西
-		echo $one_time;
-		echo "\n";
-		echo mysql_query('SELECT now()', $con);
-		echo mysql_query('SELECT TIMESTAMP($one_time)',$con);
-		$sql = 'SELECT count(*) from saying where creat_time >= timestamp($one_time) 
-				and creat_time <= date_add(timestamp($one_time), interval 30 day)';
+		echo $one_time[0];
+		if($one_time[0] == "2016-03-01"){
+			echo "right!\n";
+		}
+		$sql = "SELECT count(*) from saying where creat_time >= timestamp('$one_time[0]')";
 		//$sql = 'SELECT count(*) from saying';
-		$one_time_count = mysql_query($sql, $con);
+		$one_time_count = mysql_fetch_array(mysql_query($sql, $con));
+		echo $one_time_count[0];
 		//查询，返回计数
 		echo 
 		"
-		<p><a href = 'search_one_time.php'>($one_time_count)</a></p>
+		<p><a href = 'search_one_time.php'>($one_time_count[0])</a></p>
 		";
 		echo "\n";		
 	}
